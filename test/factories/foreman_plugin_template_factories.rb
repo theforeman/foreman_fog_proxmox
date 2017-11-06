edfcb259da2efdf3bb03d141d24ee58243687b53
Timo Goebel <mail@timogoebel.name>
2017-11-06 17:04:08 +0100

    replace FactoryGirl with FactoryBot (#25)

--- test/factories/foreman_plugin_template_factories.rb
+++ test/factories/foreman_plugin_template_factories.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :host do
     name 'foreman_plugin_template'
   end
--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -1,6 +1,6 @@
 # This calls the main test_helper in Foreman-core
 require 'test_helper'
 
-# Add plugin to FactoryGirl's paths
-FactoryGirl.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
-FactoryGirl.reload
+# Add plugin to FactoryBot's paths
+FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
+FactoryBot.reload

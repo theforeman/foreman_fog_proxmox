06118d70c6eb9cda8c7e0c679f9f5c78f43633a5
Tristan Robert <tristanr.robert.44@gmail.com>
2018-06-14 13:20:53 +0200

    Fix test coverage
    Add simplecov

--- foreman_proxmox.gemspec
+++ foreman_proxmox.gemspec
@@ -36,4 +36,5 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency 'rdoc'
   s.add_development_dependency 'rubocop'
+  s.add_development_dependency 'simplecov'
 end
--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -1,4 +1,13 @@
 # This calls the main test_helper in Foreman-core
+
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/test/'
+  add_group 'App', 'app'
+  add_group 'Lib', 'lib'
+end
+
 require 'test_helper'
 
 # Add plugin to FactoryBot's paths

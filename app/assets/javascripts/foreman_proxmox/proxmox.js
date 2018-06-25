c74a37d85417051980fbbf8f83517b788bfeeb53
Tristan Robert <tristanr.robert.44@gmail.com>
2018-06-25 15:24:49 +0200

    Fixes #14 - Fix default server config
    Add max device from controller volume selected

--- app/assets/javascripts/foreman_proxmox/proxmox.js
+++ app/assets/javascripts/foreman_proxmox/proxmox.js
@@ -5,14 +5,18 @@ function cdromSelected(item) {
 
   switch (selected) {
     case 'none':
-      $('#host_compute_attributes_config_attributes_cdrom_iso').empty();
+      initStorage();
+      initOptions('iso');
       cdrom_image_form.hide();
       break;
     case 'cdrom':
-      $('#host_compute_attributes_config_attributes_cdrom_iso').empty();
+      initStorage();
+      initOptions('iso');
       cdrom_image_form.hide();
       break;
     case 'image':
+      initStorage();
+      initOptions('iso');
       cdrom_image_form.show();
       break;
     default:
@@ -21,23 +25,70 @@ function cdromSelected(item) {
   return false;
 }
 
+function initStorage(){
+  var select = '#host_compute_attributes_config_attributes_cdrom_storage';
+  $(select + ' option:selected').prop('selected',false);
+  $(select).val('');
+}
+
+function initOptions(name){
+  var select = '#host_compute_attributes_config_attributes_cdrom_'+name;
+  $(select).empty();
+  $(select).append($("<option></option>").val('').text(''));
+  $(select).val('');
+}
+
   function storageIsoSelected(item) {
-    tfm.tools.showSpinner();
     var storage = $(item).val();
-    $.getJSON({
-      type: 'get',
-      url: '/foreman_proxmox/isos/'+storage,
-      complete: function(){
-        tfm.tools.hideSpinner();
-      },
-      error: function(j,status,error){
-        console.log("Error=" + error +", status=" + status + " loading isos for storage=" + storage);
-      },
-      success: function(isos) {
-        $('#host_compute_attributes_config_attributes_cdrom_iso').empty();
-        $.each(isos, function(i,iso){
-          $('#host_compute_attributes_config_attributes_cdrom_iso').append($("<option></option>").val(iso.volid).text(iso.volid));
-        });
-      }
-    });
+    if (storage != '') {
+      tfm.tools.showSpinner();
+      $.getJSON({
+        type: 'get',
+        url: '/foreman_proxmox/isos/'+storage,
+        complete: function(){
+          tfm.tools.hideSpinner();
+        },
+        error: function(j,status,error){
+          console.log("Error=" + error +", status=" + status + " loading isos for storage=" + storage);
+        },
+        success: function(isos) {
+          initOptions('iso');
+          $.each(isos, function(i,iso){
+            $('#host_compute_attributes_config_attributes_cdrom_iso').append($("<option></option>").val(iso.volid).text(iso.volid));
+          });
+        }
+      });
+    } else {
+      initOptions('iso');
+    }
   }
+
+function controllerSelected(item){
+  var controller = $(item).val();
+  var id = $(item).attr('id');
+  var pattern = /(\w+)(\d+)(\w+)/i;
+  var index =  pattern.exec(id)[2];
+  var max = computeControllerMaxDevice(controller);
+  $('#host_compute_attributes_volumes_attributes_' + index + '_device').attr('data-soft-max',max);
+  tfm.numFields.initAll();
+}
+
+function computeControllerMaxDevice(controller){
+  switch (controller) {
+    case 'ide':
+      return 3;
+      break;
+    case 'sata':
+      return 5;
+      break;
+    case 'scsi':
+      return 13;
+      break;
+    case 'virtio':
+      return 15;
+      break;
+    default:
+      return 1;
+      break;
+  }
+}

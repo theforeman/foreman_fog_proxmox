a3e992725b39378ab341a16091037e397666e598
Tristan Robert <tristanr.robert.44@gmail.com>
2018-06-26 17:12:30 +0200

    Fix size disk

---
+++ app/models/concerns/fog_extensions/proxmox/disk.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Copyright 2018 Tristan Robert
+
+# This file is part of ForemanProxmox.
+
+# ForemanProxmox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# ForemanProxmox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with ForemanProxmox. If not, see <http://www.gnu.org/licenses/>.
+
+module FogExtensions
+    module Proxmox
+        module Disk
+            extend ActiveSupport::Concern
+            def size
+                size * GIGA
+            end
+        end
+    end
+end   

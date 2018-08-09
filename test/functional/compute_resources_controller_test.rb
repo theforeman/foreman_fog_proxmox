8d58c55ca3daa132672bbcc0e8294f5e593e12c4
Tristan Robert <tristan.robert.44@gmail.com>
2018-08-09 13:10:36 +0200

    Add compute resources controller test

---
+++ test/functional/compute_resources_controller_test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# Copyright 2018 Tristan Robert
+
+# This file is part of ForemanFogProxmox.
+
+# ForemanFogProxmox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# ForemanFogProxmox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with ForemanFogProxmox. If not, see <http://www.gnu.org/licenses/>.
+require 'test_plugin_helper'
+
+class ::ForemanFogProxmox::ComputeResourcesControllerTest < ActionController::TestCase
+    test 'should get isos' do
+        get :isos, params: { :storage => 'local' }
+        assert_response :found
+        show_response = @response.body
+        assert !show_response.empty?
+    end
+    test 'should get ostemplates' do
+        get :ostemplates, params: { :storage => 'local' }
+        assert_response :found
+        show_response = @response.body
+        assert !show_response.empty?
+    end
+end

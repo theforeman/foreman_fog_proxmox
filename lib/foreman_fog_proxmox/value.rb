338327eb13e096318edd61a14c0be1ea44a1737e
Tristan Robert <tristan.robert.44@gmail.com>
2018-09-01 10:41:38 +0200

    Fixes Value module definition

--- lib/foreman_fog_proxmox/value.rb
+++ lib/foreman_fog_proxmox/value.rb
@@ -17,10 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with ForemanFogProxmox. If not, see <http://www.gnu.org/licenses/>.
 
-module ForemanFogProxmox
-    module Value
-        def self.empty?(value)
-            value == nil || value == ""
-        end
+module ForemanFogProxmox::Value
+    def self.empty?(value)
+        value == nil || value == ""
     end
 end

e6ac79a3f26bacfb362f7efac07cb62ef4cf3002
Bernhard Suttner <sbernhard@users.noreply.github.com>
2020-07-21 21:24:48 +0200

    Update proxmox_compute_attributes.rb

--- app/models/foreman_fog_proxmox/proxmox_compute_attributes.rb
+++ app/models/foreman_fog_proxmox/proxmox_compute_attributes.rb
@@ -54,7 +54,7 @@ module ForemanFogProxmox
       if vm.respond_to?(:config)
         vm_attrs = vm_attrs.merge(vmid: vm.identity, node_id: vm.node_id, type: vm.type)
         vm_attrs[:volumes_attributes] = Hash[vm.config.disks.each_with_index.map { |disk, idx| [idx.to_s, disk.attributes] }] if vm.config.respond_to?(:disks)
-        if vm.config.respond_to?(:interfaces):
+        if vm.config.respond_to?(:interfaces)
           vm_attrs[:interfaces_attributes] = Hash[vm.config.interfaces.each_with_index.map { |interface, idx| [idx.to_s, interface_compute_attributes(interface.attributes)] }]
         end
         vm_attrs[:config_attributes] = vm.config.attributes.reject do |key, value|

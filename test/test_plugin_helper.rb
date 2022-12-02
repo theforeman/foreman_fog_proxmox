9b2d3ea755f1257f1807b80df83585181a2f732e
Tristan Robert <tristan.robert.44@gmail.com>
2022-12-02 17:36:40 +0100

    ci: :construction_worker: fixes coverage report on codeclimate Fixes #234

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -2,14 +2,19 @@
 
 # This calls the main test_helper in Foreman-core
 
-require 'simplecov'
+if ENV['COVERAGE']
+  require 'simplecov'
 
-SimpleCov.start do
-  add_filter '/test/'
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Models', 'app/models'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Lib', 'lib'
+  SimpleCov.start do
+    SimpleCov.root('../foreman_fog_proxmox')
+    load_profile "test_frameworks"
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Overrides', 'app/overrides'
+    add_group 'Services', 'app/services'
+    add_group 'Lib', 'lib'
+  end
 end
 
 require 'test_helper'

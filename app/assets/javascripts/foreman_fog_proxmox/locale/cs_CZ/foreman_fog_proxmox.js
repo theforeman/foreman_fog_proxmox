3ec2f8e1137390d9506336fab5be4e54fea38305
Nadja Heitmann <nadjah@atix.de>
2026-02-12 07:55:18 +0000

    i18n - pulling from tx

---
+++ app/assets/javascripts/foreman_fog_proxmox/locale/cs_CZ/foreman_fog_proxmox.js
@@ -0,0 +1,646 @@
+ locales['foreman_fog_proxmox'] = locales['foreman_fog_proxmox'] || {}; locales['foreman_fog_proxmox']['cs_CZ'] = {
+  "domain": "foreman_fog_proxmox",
+  "locale_data": {
+    "foreman_fog_proxmox": {
+      "": {
+        "Project-Id-Version": "foreman_fog_proxmox 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2020-03-08 09:47+0000",
+        "Last-Translator": "Pavel Borecki <pavel.borecki@gmail.com>, 2022",
+        "Language-Team": "Czech (Czech Republic) (https://app.transifex.com/foreman/teams/114/cs_CZ/)",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Language": "cs_CZ",
+        "Plural-Forms": "nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n >= 2 && n <= 4 && n % 1 == 0) ? 1: (n % 1 != 0 ) ? 2 : 3;",
+        "lang": "cs_CZ",
+        "domain": "foreman_fog_proxmox",
+        "plural_forms": "nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n >= 2 && n <= 4 && n % 1 == 0) ? 1: (n % 1 != 0 ) ? 2 : 3;"
+      },
+      "%(name)s (free: %(free)s, used: %(used)s, total: %(total)s)": [
+        ""
+      ],
+      "%<s>s console is not supported at this time": [
+        "%<s>s v tuto chvíli není podporována"
+      ],
+      "%<vm>s is now %<vm_state>s": [
+        ""
+      ],
+      "Access ticket fields": [
+        ""
+      ],
+      "Activate AES instruction set for HW accelaration.": [
+        ""
+      ],
+      "Add CD-ROM": [
+        ""
+      ],
+      "Add Cloud-init": [
+        ""
+      ],
+      "Add Efi Disk": [
+        ""
+      ],
+      "Add Hard Disk": [
+        ""
+      ],
+      "Add HardDisk": [
+        ""
+      ],
+      "Add Interface": [
+        ""
+      ],
+      "Add Mount Point": [
+        ""
+      ],
+      "Add MountPoint": [
+        ""
+      ],
+      "Add Volume": [
+        "Přidat svazek"
+      ],
+      "Advanced": [
+        "Pokročilé"
+      ],
+      "Advanced Options": [
+        ""
+      ],
+      "Allow guest OS to use 1GB size pages, if host HW supports it": [
+        ""
+      ],
+      "Allows improved Spectre mitigation with AMD CPUs": [
+        ""
+      ],
+      "Allows improved Spectre mitigation with Intel CPUs": [
+        ""
+      ],
+      "Architecture": [
+        "Architektura"
+      ],
+      "Associate VM": [
+        ""
+      ],
+      "Associate VM to a Foreman host": [
+        "Přiřadit virt. stroj k Foreman hostiteli"
+      ],
+      "Authentication method": [
+        ""
+      ],
+      "Average load (x100)": [
+        "Průměrné vytížení (x100)"
+      ],
+      "BIOS": [
+        "BIOS"
+      ],
+      "Basis for \\\\\\\"Speculative Store Bypass\\\\\\\" protection for AMD models": [
+        ""
+      ],
+      "Boot device order": [
+        "Pořadí zavádění ze zařízení"
+      ],
+      "Bridge": [
+        "Most"
+      ],
+      "CD-ROM": [
+        "Jednotka optických disků"
+      ],
+      "CIDR IPv4": [
+        ""
+      ],
+      "CIDR IPv6": [
+        ""
+      ],
+      "CPU": [
+        "Procesor"
+      ],
+      "CPU Flags": [
+        ""
+      ],
+      "CPU limit": [
+        "Omezení procesorů"
+      ],
+      "CPU units": [
+        "Jednotek procesorů"
+      ],
+      "CPUs": [
+        "Procesory"
+      ],
+      "Cache": [
+        "Mezipaměť"
+      ],
+      "Card": [
+        "Karta"
+      ],
+      "Change the password": [
+        "Změnit heslo"
+      ],
+      "Click Test connection button before changing it": [
+        ""
+      ],
+      "Click Test connection button to check token": [
+        ""
+      ],
+      "Cloud-init": [
+        ""
+      ],
+      "Cluster nodes": [
+        "Uzly klastru"
+      ],
+      "Common fields": [
+        ""
+      ],
+      "Confirm": [
+        ""
+      ],
+      "Console": [
+        "Konzola"
+      ],
+      "Controller": [
+        "Řadič"
+      ],
+      "Cores": [
+        "Jádra"
+      ],
+      "Create image?": [
+        "Vytvořit obraz?"
+      ],
+      "DHCP IPv4": [
+        "DHCP IPv4"
+      ],
+      "DHCP IPv6": [
+        "DHCP IPv6"
+      ],
+      "DNS": [
+        "DNS"
+      ],
+      "DNS domain": [
+        ""
+      ],
+      "DNS server": [
+        "DNS server"
+      ],
+      "DNS servers": [
+        ""
+      ],
+      "Description": [
+        "Popis"
+      ],
+      "Device": [
+        "Zařízení"
+      ],
+      "Device value is set automatically.": [
+        ""
+      ],
+      "Disconnect": [
+        "Odpojit"
+      ],
+      "Does this image support user data input (e.g. via cloud-init)?": [
+        "Podporuje tento obraz zadání uživatelských dat (např. prostřednictvím cloud-init)?"
+      ],
+      "EFI Disk": [
+        ""
+      ],
+      "EFI Storage": [
+        ""
+      ],
+      "Enable NUMA": [
+        "Zapnout NUMA"
+      ],
+      "Enable/disable KVM hardware virtualization": [
+        "Vyp/zap hardware virtualizaci KVM "
+      ],
+      "Error - %<message>s": [
+        ""
+      ],
+      "Error: Duplicate ID found.": [
+        ""
+      ],
+      "Error: Duplicate Name found.": [
+        ""
+      ],
+      "Extended": [
+        "Rozšířeno"
+      ],
+      "Extra CPU Flags": [
+        ""
+      ],
+      "Failed fetching images ISO please try again.": [
+        ""
+      ],
+      "Failed fetching templates please try again.": [
+        ""
+      ],
+      "Failed retrieving proxmox server vm by vmid=%<vmid>s": [
+        "Nepodařilo se získat virt. stroj na proxmox serveru podle vmid=%<vmid>s"
+      ],
+      "Failed to acquire IP addresses from compute resource for %<name>s": [
+        ""
+      ],
+      "Failed to load Proxmox metadata. Please check your compute resource connection.": [
+        ""
+      ],
+      "Failed to set console: %s": [
+        "Nepodařilo se nastavit konzoli: %s"
+      ],
+      "Failed to undo update compute %<compute_resource>s instance %<name>s: %<e>s": [
+        "Nepodařilo se vzít zpět výpočetní %<compute_resource>s instance %<name>s: %<e>s"
+      ],
+      "Failed to update a compute %<compute_resource>s instance %<name>s: %<e>s": [
+        "Nepodařilo se aktualizovat výpočetní %<compute_resource>s instance %<name>s: %<e>s"
+      ],
+      "Fetching images ISO...": [
+        ""
+      ],
+      "Fetching templates...": [
+        ""
+      ],
+      "Firewall": [
+        "Brána firewall"
+      ],
+      "Foreman Fog Proxmox widget": [
+        "Ovládací prvek Foreman Fog Proxmox"
+      ],
+      "ForemanFogProxmox plugin adds Proxmox VE compute resource to Foreman using fog-proxmox gem.": [
+        "Zásuvný modul ForemanFogProxmox do Foreman jako výpočetní prostředek Proxmox VE (využívá fog-proxmox rubby gem)."
+      ],
+      "Gateway IPv4": [
+        "Brána IPv4"
+      ],
+      "Gateway IPv6": [
+        "Brána IPv6"
+      ],
+      "General": [
+        "Obecné"
+      ],
+      "Hard Disk %(hddId)s": [
+        ""
+      ],
+      "Hard disk": [
+        "Pevný disk"
+      ],
+      "Hardware": [
+        ""
+      ],
+      "Host": [
+        "Stroj"
+      ],
+      "Hostname": [
+        "Název stroje"
+      ],
+      "Identifier": [
+        "Identifikátor"
+      ],
+      "Image": [
+        "Obraz"
+      ],
+      "Image ISO": [
+        "ISO obraz"
+      ],
+      "Image VMID": [
+        "Identif. virtuálního stroje daného obrazu"
+      ],
+      "Import as managed Host": [
+        "Importovat jako spravovaný stroj"
+      ],
+      "Import as unmanaged Host": [
+        "Importovat jako nespravovaný stroj"
+      ],
+      "Improve performance for nested virtualization. Only supported on Intel CPUs.": [
+        ""
+      ],
+      "Improve performance in overcommitted Windows guests. May lead to guest bluescreens on old CPUs.": [
+        ""
+      ],
+      "Improves Spectre mitigation performance with AMD CPUs, best used with \\\\\\\"virt-ssbd\\\\\\\"": [
+        ""
+      ],
+      "KVM": [
+        "KVM"
+      ],
+      "Main option": [
+        "Hlavní volba",
+        "Hlavní volby",
+        "Hlavních voleb",
+        "Hlavní volby"
+      ],
+      "Media": [
+        "Médium"
+      ],
+      "Meltdown fix cost reduction on Westmere, Sandy-, and IvyBridge Intel CPUs": [
+        ""
+      ],
+      "Memory": [
+        "Operační paměť"
+      ],
+      "Memory (MB)": [
+        "Paměť (MB)"
+      ],
+      "Minimum Memory (MB)": [
+        ""
+      ],
+      "Minimum memory (MB)": [
+        ""
+      ],
+      "Mount Point %(mp)s": [
+        ""
+      ],
+      "Mount point": [
+        "Přípojný bod"
+      ],
+      "Multiqueue": [
+        ""
+      ],
+      "Name": [
+        "Název"
+      ],
+      "Network Interfaces": [
+        ""
+      ],
+      "Nic": [
+        "Síť. rozhraní"
+      ],
+      "Nic %(nicId)s": [
+        ""
+      ],
+      "No host found to associate this VM with": [
+        "Nenalezen žádný hostitel ke kterému přiřadit tento virt. stroj"
+      ],
+      "Node": [
+        "Uzel"
+      ],
+      "None": [
+        "Žádné"
+      ],
+      "Notifies guest OS that host is not vulnerable for Spectre on AMD CPUs": [
+        ""
+      ],
+      "OS Template": [
+        "Šablona operačního systému"
+      ],
+      "OS Type": [
+        ""
+      ],
+      "OS type": [
+        "Typ operačního systému"
+      ],
+      "Operating System": [
+        "Operační systém"
+      ],
+      "Operating system family %<type>s is not consistent with %<ostype>s": [
+        "Generace operačního systému %<type>s není konzistentní s %<ostype>s"
+      ],
+      "Optionally provide a CA, or a correctly ordered CA chain. If left blank, disable ssl_verify_peer.": [
+        "Volitelně zadejte cer. autoritu, nebo správně řazený řetězec cert. autirt. Pokud nebude vyplněno, vypněte ssl_verify_peer."
+      ],
+      "Order your devices, e.g. order=net0;ide2;scsi0. Default empty (any)": [
+        ""
+      ],
+      "Password": [
+        "Heslo"
+      ],
+      "Password to authenticate with - used for SSH finish step.": [
+        "Heslo které použít pro ověření se – použito pro krok dokončení po SSH."
+      ],
+      "Path": [
+        "Popis umístění"
+      ],
+      "Path cannot be empty": [
+        ""
+      ],
+      "Physical": [
+        "Fyzický"
+      ],
+      "Pool": [
+        "Fond"
+      ],
+      "Power": [
+        "Napájení"
+      ],
+      "Pre-Enrolled Keys": [
+        ""
+      ],
+      "Protection for \\\\\\\"Speculative Store Bypass\\\\\\\" for Intel models": [
+        ""
+      ],
+      "Proxmox Node Statistics: %s": [
+        "Statistiky Proxmox uzlu: %s"
+      ],
+      "Proxmox version %<version>s is not semver suitable": [
+        "Verze proxmox %<version>s není vhodná pro semver"
+      ],
+      "Qemu Agent": [
+        "Qemu agent"
+      ],
+      "Rate limit": [
+        "Limit rychlosti"
+      ],
+      "Reached maximum number of devices for controller %(value)s. Please select another controller.": [
+        ""
+      ],
+      "Required to let the guest OS know if MDS is mitigated correctly": [
+        ""
+      ],
+      "Reset": [
+        ""
+      ],
+      "Root Password": [
+        ""
+      ],
+      "Root password": [
+        "Heslo správce (root)"
+      ],
+      "Rootfs": [
+        "Kořen. souborový systém"
+      ],
+      "SCSI Controller": [
+        "SCSI řadič"
+      ],
+      "SSH public key": [
+        ""
+      ],
+      "SSL fields": [
+        ""
+      ],
+      "SSL verify peer": [
+        "SSL ověřit protějšek"
+      ],
+      "Search Domain": [
+        ""
+      ],
+      "Search domain": [
+        "Hledat v doménách"
+      ],
+      "Shares (MB)": [
+        ""
+      ],
+      "Size (GB)": [
+        "Velikost (GB)"
+      ],
+      "Sockets": [
+        "Patice"
+      ],
+      "Start after creation?": [
+        "Spustit po vytvoření?"
+      ],
+      "Start at boot": [
+        "Spustit při zavádění systému"
+      ],
+      "Storage": [
+        "Úložiště"
+      ],
+      "Swap (MB)": [
+        ""
+      ],
+      "Template Storage": [
+        ""
+      ],
+      "Template storage": [
+        "Úložiště šablon"
+      ],
+      "The EFI Disk requires OVMF BIOS. Please switch the BIOS type to OVMF.": [
+        ""
+      ],
+      "The user that is used to ssh into the instance, normally cloud-user, ubuntu, root etc": [
+        "Uživatelský účet, kterým navázat ssh spojení s instancí (obyčejně cloud-user, ubuntu, root, atd.)"
+      ],
+      "Time": [
+        "Čas"
+      ],
+      "Type": [
+        "Typ"
+      ],
+      "Type %<type>s, node %<node>s, %<cpus>s CPUs and %<memory>s MB memory": [
+        ""
+      ],
+      "URL": [
+        "URL adresa"
+      ],
+      "Unable to shrink %<id>s size. Proxmox allows only increasing size.": [
+        "Velikost %<id>s nelze zmenšit. Proxmox umožňuje pouze zvětšování."
+      ],
+      "Unable to store X509 certificates": [
+        "Nepodařilo se uložit x509 certifikáty"
+      ],
+      "Uptime": [
+        "Doba chodu"
+      ],
+      "User": [
+        "Uživatel"
+      ],
+      "User token expires": [
+        ""
+      ],
+      "User token fields": [
+        ""
+      ],
+      "User token id": [
+        ""
+      ],
+      "User token value": [
+        ""
+      ],
+      "VCPUs": [
+        "Virt. procesory"
+      ],
+      "VGA": [
+        "grafika"
+      ],
+      "VLAN Tag": [
+        ""
+      ],
+      "VLAN tag": [
+        "VLAN štítek"
+      ],
+      "VM ID": [
+        "Identif. virtuálního stroje"
+      ],
+      "VM already associated with a host": [
+        "Virt. stroj už je přiřazený k hostiteli"
+      ],
+      "VM associated to host %s": [
+        "Vírt. stro přiřazen k hostiteli %s"
+      ],
+      "Version": [
+        "Verze"
+      ],
+      "X509 Certification Authorities": [
+        "X509 certifikační autority"
+      ],
+      "add new cd-rom": [
+        ""
+      ],
+      "add new cloud-init": [
+        ""
+      ],
+      "add new hard disk": [
+        ""
+      ],
+      "add new mount point": [
+        ""
+      ],
+      "add new storage volume": [
+        "přidat nový svazek úložiště"
+      ],
+      "e.g. /path/to/": [
+        "např. /popis/umisteni/"
+      ],
+      "e.g. https://127.0.0.1:8006/api2/json": [
+        "např. https://127.0.0.1:8006/api2/json"
+      ],
+      "e.g. root@pam": [
+        "např. root@pam"
+      ],
+      "eth[n] with n integer >= 0, e.g. eth0": [
+        ""
+      ],
+      "failed to %<action>s %<vm>s": [
+        ""
+      ],
+      "failed to save %<name>s": [
+        ""
+      ],
+      "integer within [0..128]": [
+        ""
+      ],
+      "integer within [0..32]": [
+        ""
+      ],
+      "invalid vmid=%<vmid>s": [
+        "neplatný vmid=%<vmid>s"
+      ],
+      "net[n] with n integer >= 0, e.g. net0": [
+        ""
+      ],
+      "new_typed_vm(%<type>s) with vm_typed_instance_defaults: vm_h=%<vm_h>s": [
+        ""
+      ],
+      "parse_efidisk(): args=%<args>s": [
+        ""
+      ],
+      "parse_hard_disk_volume(): args=%<args>s": [
+        ""
+      ],
+      "remove CDROM": [
+        ""
+      ],
+      "remove Cloud-init": [
+        ""
+      ],
+      "remove Hard disk": [
+        ""
+      ],
+      "remove mount point": [
+        ""
+      ],
+      "remove network interface": [
+        "odebrat síťové rozhraní"
+      ],
+      "remove storage volume": [
+        "odebrat svazek úložiště"
+      ],
+      "vmid of template vm.": [
+        "identif. virtuálního stroje šablony virt. stroje."
+      ]
+    }
+  }
+};
